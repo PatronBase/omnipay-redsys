3d96aa313f6cf077593137d7c1c9798805117bcf
Leith Caldwell <leith@codedruids.com>
2025-07-21 12:18:33 +1200

    Merge POST & GET data for CompletePurchase (fixes #4)

--- src/Message/CompletePurchaseRequest.php
+++ src/Message/CompletePurchaseRequest.php
@@ -9,7 +9,7 @@ class CompletePurchaseRequest extends PurchaseRequest
 {
     public function getData()
     {
-        return $this->httpRequest->request->all();
+        return $this->httpRequest->request->all() + $this->httpRequest->query->all();
     }
 
     public function sendData($data)
